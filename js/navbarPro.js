f08b3d849b863ca0e29f284061d4166567aefcdf
vitoriazoche <vitoriazoche@gmail.com>
2019-10-17 17:06:22 -0300

    :bento:

--- js/navbarPro.js
+++ js/navbarPro.js
@@ -1,8 +1,8 @@
-filterSelection("all")
+filterSelection("todos")
 function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
+  if (c == "todos") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");

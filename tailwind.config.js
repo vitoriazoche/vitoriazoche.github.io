d36d1130251482c2cc133337a0b1e1e57eac763c
vitoriazoche <vitoriazoche@gmail.com>
2022-07-05 20:29:16 -0300

    :ambulance:

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["/src/**/*.{html,js}"],
+  content: [
+   "./public/index.html"
+  ],
   theme: {
+    extend: {},
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
